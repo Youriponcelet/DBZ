5eecd9dc5b0f463cd3d3665e74c272bde983a00c
Youriponcelet <youri.poncelet@live.fr>
2016-02-19 18:09:25 +0100

    final

--- Classes/model.class.php
+++ Classes/model.class.php
@@ -28,7 +28,7 @@ class Model
 	// list data table
 	public function EntitiesTable($Name_Table)
 		{
-		$SQL = "select * from " . $Name_Table;
+		$SQL = "select * from ".$Name_Table;
 		$RES = $this->PDO->prepare($SQL);
 		$RES->execute();
 		return $RES->fetchAll();
@@ -37,10 +37,33 @@ class Model
 	// delete the table
 	public function DeleteTable($Name_Table)
 		{
-		$REQ = "delete from ".$Name_Table;
+		$REQ = "DROP TABLE ".$Name_Table;
 		$RES = $this->PDO->prepare($REQ);
 		$RES->execute();
 		}
-	  
+
+	/*if(isset($POST['sendmodify'])) //if you send the form to update an entities
+		{	
+		// call the function mod_entites
+		public function MOD_ENTITIES($Name_Table,$entities,$value)
+			{
+			// foreact value, make the request "UPDATE TABLE SET ENTITIES = VALUE"
+			$REQ = "update " . $Name_Table . " set " . $entities . " = " . $value;
+			$RES = $this->PDO->prepare($REQ);
+			$RES->execute();
+			}	
+		}  
+
+	if(isset($POST['sendmodify'])) //if you send the form to add an entities
+		{	
+		// call the function add_entites
+		public function ADD_ENTITIES($Name_Table,$entities,$value)
+			{
+			// INSERT INTO table each value
+			$REQ = "INSERT INTO " . $Name_Table . " set " . $entities . " = " . $value;
+			$RES = $this->PDO->prepare($REQ);
+			$RES->execute();
+			}	
+		}  */
 	}
 ?>
--- Classes/view.class.php
+++ Classes/view.class.php
@@ -22,8 +22,11 @@ class View
     //data table
     public static function DataTable($db_name, $array_data)
 		{
-		$list = "<table>"; //open table
+		$list = "<a href='?T=".$_GET["T"]."&amp;REQ=DEL_TABLE'>- DELETE TABLE ".strtoupper($_GET["T"])."</a><br>";
+		$list .= "<a href='?T=".$_GET["T"]."&amp;REQ=ADD_ENTITIES'>- ADD ENTITIES TO ".strtoupper($_GET["T"])."</a><br>";
+		$list .= "<table>"; //open table
 		$i = 0; //iteraction counter
+		$a = 0;
       
 		//get table name
 		foreach ($array_data[0] as $key => $value) 
@@ -31,6 +34,8 @@ class View
 			if($i%2 == 0)
 				{
 				$list .= "<th>".$key."</th>";
+				$titre[$a]=$key;
+				$a++;
 				}        
 			$i++;
 			}
@@ -42,10 +47,12 @@ class View
 			for($i = 0; $i < COUNT($DATA)/2; $i++)
 				{
 				$list .= "<td>".$DATA[$i]."</td>";
-				}        
+				}   
+			$list .= "<td><a href='?ID=".$DATA[0]."&amp;PRIMARY=".$titre[0]."&amp;T=".$_GET["T"]."&amp;REQ=UPDATE_DATA'><img src='./Fichiers/images/modifier.png'></a></td>"; 
+			$list .= "<td><a href='?ID=".$DATA[0]."&amp;PRIMARY=".$titre[0]."&amp;T=".$_GET["T"]."&amp;REQ=DEL_DATA'><img src='./Fichiers/images/supprimer.png'></a></td>";   
 			$list .= "</tr>"; // close row
 			}      
-		$list .= "</table>";  //close table   
+		$list .= "</table>";  //close table  
 		return $list;      
 		}   
 		
@@ -63,11 +70,65 @@ class View
 					<link rel='stylesheet' type='text/css' href='Fichiers/css/style.css' />
 				</head>
 				<body>
-					<img src='Fichiers/images/logo.jpg' height='80'/><br /><hr />
+					<img src='Fichiers/images/logo.jpg' height='80'/><br />
+					<p>TODO: You must reload the page to see the changes.</p><br />
+					<p>TODO: ADD/MODIFY ENTITIES are not functionnal, only form it working.</p><hr />
 					</hr>".$contener."
 				</body>
 			</html>";
 		}
+
+	public static function DEL_TABLE() 
+		{
+	      echo "La table a bien été supprimée ";
+	    }
+
+    public static function DEL_DATA() 
+		{
+       echo "La ligne a bien été supprimée ";
+    	}
+
+    public static function MOD_ENTITIES($db_name, $array_data) 
+		{
+		$form = "<form method='POST' action='model.class.php'>";
+		$i = 0; //iteraction counter
+		$a = 0;
+
+		foreach ($array_data[0] as $key => $value) 
+			{
+			if($i%2 == 0)
+				{
+				$form .= $key;
+				$form .= "<input type='text' name='".$key."'' value='".$value."'>";
+				}        
+			$i++;
+			}    
+		$form .= "<input type='submit' name='sendmodify' value='send'>";
+		$form .= "</form>";
+
+		return $form;
+        }  
+
+    public static function ADD_ENTITIES($db_name, $array_data) 
+		{
+		$form = "<form method='POST' action='model.class.php'>";
+		$i = 0; //iteraction counter
+		$a = 0;
+
+		foreach ($array_data[0] as $key => $value) 
+			{
+			if($i%2 == 0)
+				{
+				$form .= $key;
+				$form .= "<input type='text' name='".$key."'>";
+				}        
+			$i++;
+			}    
+		$form .= "<input type='submit' name='sendadd' value='send'>";
+		$form .= "</form>";
+
+		return $form;
+    	} 
     
 	}
 ?>
